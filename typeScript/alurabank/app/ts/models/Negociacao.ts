c77c0096523a62a34d58ca8bef64549ef34e9c51
Sara Cristine Tiedt (GOVBR MTZ - DDP Arquitetura) <Sara.Tiedt@govbr.com.br>
2020-12-01 22:56:24 -0300

    Aula 2

--- typeScript/alurabank/app/ts/models/Negociacao.ts
+++ typeScript/alurabank/app/ts/models/Negociacao.ts
@@ -1,34 +1,23 @@
 class Negociacao {
+  constructor(
+    private _data: Date,
+    private _quantidade: number,
+    private _valor: number
+  ) {}
 
-    private _data;
-    private _quantidade;
-    private _valor;
-    
-    constructor(data, quantidade, valor) {
+  get data() {
+    return this._data;
+  }
 
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+  get quantidade() {
+    return this._quantidade;
+  }
 
-    get data() {
+  get valor() {
+    return this._valor;
+  }
 
-        return this._data;
-    }
-
-    get quantidade() {
-
-        return this._quantidade;
-    }
-
-    get valor() {
-
-        return this._valor;
-    }
-
-    get volume() {
-
-        return this._quantidade * this._valor;
-    }
+  get volume() {
+    return this._quantidade * this._valor;
+  }
 }
-
