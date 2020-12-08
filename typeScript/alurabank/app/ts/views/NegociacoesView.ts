f94e4498f480b4efbf46c4072a5df7d06d3c9f79
Sara Cristine Tiedt (GOVBR MTZ - DDP Arquitetura) <Sara.Tiedt@govbr.com.br>
2020-12-07 21:55:36 -0300

    aula 4

--- typeScript/alurabank/app/ts/views/NegociacoesView.ts
+++ typeScript/alurabank/app/ts/views/NegociacoesView.ts
@@ -1,13 +1,4 @@
-class NegociacoesView {
-  private _elemento: Element;
-  constructor(seletor: string) {
-    this._elemento = document.querySelector(seletor);
-  }
-
-  update(model: Negociacoes): void {
-    this._elemento.innerHTML = this.template(model);
-  }
-
+class NegociacoesView extends View<Negociacoes> {
   template(model: Negociacoes): string {
     return `
 <table class="table table-hover table-bordered">
@@ -21,16 +12,22 @@ class NegociacoesView {
     </thead>
 
     <tbody>
-    ${model.paraArray().map(negociacao => 
-         `
+    ${model
+      .paraArray()
+      .map(
+        (negociacao) =>
+          `
         <tr>
-        <td>${negociacao.data.getDate()}/${negociacao.data.getMonth()+1}/${negociacao.data.getFullYear()}</td>
+        <td>${negociacao.data.getDate()}/${
+            negociacao.data.getMonth() + 1
+          }/${negociacao.data.getFullYear()}</td>
         <td>${negociacao.quantidade}</td>
         <td>${negociacao.valor}</td>
         <td>${negociacao.volume}</td>
         </tr>
         `
-    ).join('')}
+      )
+      .join("")}
     </tbody>
 
     <tfoot>
