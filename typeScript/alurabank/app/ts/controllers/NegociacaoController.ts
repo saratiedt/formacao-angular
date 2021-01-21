eee030635bd2bec44ec340114054afdd4a8d3d6e
saratiedt <saracristinetiedt@gmail.com>
2021-01-20 22:46:15 -0300

    aula 3 decorators

--- typeScript/alurabank/app/ts/controllers/NegociacaoController.ts
+++ typeScript/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,59 +1,60 @@
-import { NegociacoesView, MensagemView } from '../views/index';
-import { Negociacao, Negociacoes } from '../models/index';
+import { NegociacoesView, MensagemView } from "../views/index";
+import { Negociacao, Negociacoes } from "../models/index";
+import { domInject } from "../helpers/decorators/index";
 
 export class NegociacaoController {
+  @domInject("#data")
+  private _inputData: JQuery;
+  @domInject("#datquantidadea")
+  private _inputQuantidade: JQuery;
+  @domInject("#valor")
+  private _inputValor: JQuery;
+  private _negociacoes = new Negociacoes();
+  private _negociacoesView = new NegociacoesView("#negociacoesView");
+  private _mensagemView = new MensagemView("#mensagemView");
 
-    private _inputData: JQuery;
-    private _inputQuantidade: JQuery;
-    private _inputValor: JQuery;
-    private _negociacoes = new Negociacoes();
-    private _negociacoesView = new NegociacoesView('#negociacoesView');
-    private _mensagemView = new MensagemView('#mensagemView');
+  constructor() {
+    this._negociacoesView.update(this._negociacoes);
+  }
 
-    constructor() {
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
-        this._negociacoesView.update(this._negociacoes);
+  adiciona(event: Event) {
+    event.preventDefault();
+
+    let data = new Date(this._inputData.val().replace(/-/g, ","));
+
+    if (!this._ehDiaUtil(data)) {
+      this._mensagemView.update(
+        "Somente negociações em dias útFeis, por favor!"
+      );
+      return;
     }
 
-    adiciona(event: Event) {
+    const negociacao = new Negociacao(
+      data,
+      parseInt(this._inputQuantidade.val()),
+      parseFloat(this._inputValor.val())
+    );
 
-        event.preventDefault();
+    this._negociacoes.adiciona(negociacao);
 
-        let data = new Date(this._inputData.val().replace(/-/g, ','));
+    this._negociacoesView.update(this._negociacoes);
+    this._mensagemView.update("Negociação adicionada com sucesso!");
+  }
 
-        if(!this._ehDiaUtil(data)) {
-
-            this._mensagemView.update('Somente negociações em dias úteis, por favor!');
-            return 
-        }
-
-        const negociacao = new Negociacao(
-            data, 
-            parseInt(this._inputQuantidade.val()),
-            parseFloat(this._inputValor.val())
-        );
-
-        this._negociacoes.adiciona(negociacao);
-
-        this._negociacoesView.update(this._negociacoes);
-        this._mensagemView.update('Negociação adicionada com sucesso!');
-    }
-
-    private _ehDiaUtil(data: Date) {
-
-        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
-    }
+  private _ehDiaUtil(data: Date) {
+    return (
+      data.getDay() != DiaDaSemana.Sabado &&
+      data.getDay() != DiaDaSemana.Domingo
+    );
+  }
 }
 
 enum DiaDaSemana {
-
-    Domingo, 
-    Segunda, 
-    Terca, 
-    Quarta, 
-    Quinta, 
-    Sexta, 
-    Sabado
+  Domingo,
+  Segunda,
+  Terca,
+  Quarta,
+  Quinta,
+  Sexta,
+  Sabado,
 }
--- typeScript/alurabank/app/ts/views/View.ts
+++ typeScript/alurabank/app/ts/views/View.ts
@@ -1,22 +1,21 @@
+import { logarTempoDeExecucao } from "../helpers/decorators/index";
+
 export abstract class View<T> {
+  protected _elemento: JQuery;
+  private _escapar: boolean;
 
-    protected _elemento: JQuery;
-    private _escapar: boolean;
+  constructor(seletor: string, escapar: boolean = false) {
+    this._elemento = $(seletor);
+    this._escapar = escapar;
+  }
 
-    constructor(seletor: string, escapar: boolean = false) {
+  @logarTempoDeExecucao(true)
+  update(model: T) {
+    let template = this.template(model);
+    if (this._escapar)
+      template = template.replace(/<script>[\s\S]*?<\/script>/g, "");
+    this._elemento.html(template);
+  }
 
-        this._elemento = $(seletor);
-        this._escapar = escapar;
-    }
-
-    update(model: T) {
-        
-        let template = this.template(model);
-        if(this._escapar)
-            template = template.replace(/<script>[\s\S]*?<\/script>/g, '');
-        this._elemento.html(template);
-    }
-
-    abstract template(model: T): string;
-
+  abstract template(model: T): string;
 }
