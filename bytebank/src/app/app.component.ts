d47a8fe93caa9e066e9945589f3d22b5df72cbf2
saratiedt <saracristinetiedt@gmail.com>
2021-07-15 12:09:51 -0300

    eventEmmiter

--- bytebank/src/app/app.component.ts
+++ bytebank/src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bytebank';
+
+  destino: number;
+  valor: number;
+  transferencia: any;
+
+  transferir(evento) {
+    console.log(evento);
+    this.transferencia = evento;
+  }
 }
--- bytebank/src/app/nova-transferencia/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia/nova-transferencia.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent implements OnInit {
+  @Output() aoTransferir = new EventEmitter<any>();
+  valor: number;
+  destino: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
   }
 
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
