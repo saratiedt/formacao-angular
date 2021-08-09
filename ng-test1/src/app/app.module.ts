df5070455591acb6a92ec25b8f200fa59f65595e
Sara Cristine Tiedt <sara.tiedt@capco.com>
2021-08-09 12:08:23 -0300

    finalizado aula tres testes

--- ng-test1/src/app/app.module.ts
+++ ng-test1/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LikeWidgetModule } from './shared/components/like-widget/like-widget/like-widget.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LikeWidgetModule
   ],
   providers: [],
   bootstrap: [AppComponent]
