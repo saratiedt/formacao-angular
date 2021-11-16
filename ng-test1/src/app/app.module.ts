856c3fcc0d8c8117c4c18a4119e2724e9f7e62e3
saratiedt <saracristinetiedt@gmail.com>
2021-11-16 17:46:27 -0300

    modulo 5

--- ng-test1/src/app/app.module.ts
+++ ng-test1/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LikeWidgetModule } from './shared/components/like-widget/like-widget/like-widget.module';
+import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
